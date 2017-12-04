0bc8b1271ce0fb7492b706ff9d769a486f23f642
Michael Farber <farbohydrate@gmail.com>
2017-12-04 14:06:35 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  artist: "aerosmith",
+  song: "walk this way"
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist.artist = artist;
+  playlist.song = song;
+  return playlist;
+}
